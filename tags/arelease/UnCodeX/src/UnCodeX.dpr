46255ad10a969edef3a58c99c118cc210e229642
nobody <nobody@636d535d-a5df-456e-a4a7-8a432e46c328>
2003-03-06 15:21:06 +0000

    This commit was manufactured by cvs2svn to create tag 'arelease'.

---
+++ tags/arelease/UnCodeX/src/UnCodeX.dpr
@@ -0,0 +1,27 @@
+program UnCodeX;
+
+uses
+  Forms,
+  unit_main in 'unit_main.pas' {frm_UnCodeX},
+  unit_packages in 'unit_packages.pas',
+  unit_parser in 'unit_parser.pas',
+  unit_htmlout in 'unit_htmlout.pas',
+  unit_uclasses in 'unit_uclasses.pas',
+  unit_settings in 'unit_settings.pas' {frm_Settings},
+  unit_definitions in 'unit_definitions.pas',
+  unit_analyse in 'unit_analyse.pas',
+  unit_copyparser in 'unit_copyparser.pas',
+  Hashes in 'Hashes.pas',
+  unit_treestate in 'unit_treestate.pas',
+  unit_about in 'unit_about.pas' {frm_About},
+  unit_mshtmlhelp in 'unit_mshtmlhelp.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'UnCodeX';
+  Application.CreateForm(Tfrm_UnCodeX, frm_UnCodeX);
+  Application.CreateForm(Tfrm_About, frm_About);
+  Application.Run;
+end.
