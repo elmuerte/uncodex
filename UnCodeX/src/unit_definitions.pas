a047815c85d0dc83412f4310fdbd2d0537755eeb
Michiel Hendriks <elmuerte@users.noreply.github.com>
2004-01-11 15:33:06 +0000

    fixed incorrect keyword list files

--- UnCodeX/src/unit_definitions.pas
+++ UnCodeX/src/unit_definitions.pas
@@ -3,7 +3,7 @@
  Author:    elmuerte
  Copyright: 2003 Michiel 'El Muerte' Hendriks
  Purpose:   General definitions
- $Id: unit_definitions.pas,v 1.76 2003-12-24 11:16:19 elmuerte Exp $
+ $Id: unit_definitions.pas,v 1.77 2004-01-11 15:33:06 elmuerte Exp $
 -----------------------------------------------------------------------------}
 {
     UnCodeX - UnrealScript source browser & documenter
@@ -54,7 +54,7 @@ type
 
 const
   APPTITLE = 'UnCodeX';
-  APPVERSION = '159';
+  APPVERSION = '160';
   {$IFDEF MSWINDOWS}
   APPPLATFORM = 'MS Windows';
   {$ENDIF}
@@ -333,7 +333,7 @@ initialization
       while (Keywords1.Next) do begin
         if (Keywords1.CurrentKey <> '') then sl.Add(Keywords1.CurrentKey)
       end;
-      sl.SaveToFile(ExtractFilePath(ParamStr(0))+KEYWORDFILE2);
+      sl.SaveToFile(ExtractFilePath(ParamStr(0))+KEYWORDFILE1);
     finally
       sl.Free;
     end;
