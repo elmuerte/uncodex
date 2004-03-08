f1d7cee7cd036ca31fc0d4c0c3f2794c065d99d0
Michiel Hendriks <elmuerte@users.noreply.github.com>
2004-03-08 20:02:24 +0000

    Initial and trailing '*' in block comments are now ignored (e.g. "/******* some text ******/" results in "some text" ) Fixed tabs in block comments Fixed tabs to spaces routine in html output Style fix for link-less types in the html output

--- UnCodeX/src/unit_definitions.pas
+++ UnCodeX/src/unit_definitions.pas
@@ -3,7 +3,7 @@
  Author:    elmuerte
  Copyright: 2003, 2004 Michiel 'El Muerte' Hendriks
  Purpose:   General definitions
- $Id: unit_definitions.pas,v 1.83 2004-03-07 13:57:28 elmuerte Exp $
+ $Id: unit_definitions.pas,v 1.84 2004-03-08 20:02:24 elmuerte Exp $
 -----------------------------------------------------------------------------}
 {
     UnCodeX - UnrealScript source browser & documenter
@@ -54,7 +54,7 @@ type
 
 const
   APPTITLE = 'UnCodeX';
-  APPVERSION = '165';
+  APPVERSION = '166';
   {$IFDEF MSWINDOWS}
   APPPLATFORM = 'MS Windows';
   {$ENDIF}
