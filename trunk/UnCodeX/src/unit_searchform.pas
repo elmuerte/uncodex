f9ecd029785a11b5df48172b3899dc3a3df5e429
elmuerte <elmuerte@636d535d-a5df-456e-a4a7-8a432e46c328>
2004-03-20 20:56:08 +0000

    improved search dialog and features switch tree menu option (ctrl+tab)

--- trunk/UnCodeX/src/unit_searchform.pas
+++ trunk/UnCodeX/src/unit_searchform.pas
@@ -3,7 +3,7 @@
  Author:    elmuerte
  Copyright: 2003 Michiel 'El Muerte' Hendriks
  Purpose:   Search form, much better than the previous version
- $Id: unit_searchform.pas,v 1.4 2003-11-04 19:35:27 elmuerte Exp $
+ $Id: unit_searchform.pas,v 1.5 2004-03-20 20:56:08 elmuerte Exp $
 -----------------------------------------------------------------------------}
 {
     UnCodeX - UnrealScript source browser & documenter
@@ -30,34 +30,47 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, Buttons;
+  Dialogs, StdCtrls, Buttons, ExtCtrls, unit_uclasses, ComCtrls;
 
 type
   TClassSearch = record
-    caption:      string;
-    text:         string;
     query:        string;
     history:      TStrings;
-    isRegex:      boolean;
-    isBodySearch: boolean;
-    isStrict:     boolean;
-    isFromTop:    boolean;
+    ftshistory:   TStrings;
     Wrapped:      boolean;
+    // config
+    isFTS:				boolean;
+    // class search
+    isFromTop:		boolean;
+    isStrict:			boolean;
+    // fts
+    isRegex:			boolean;
+    isFindFirst:	boolean;
+    Scope:				integer;
+
+    // find next info
+    searchtree:			TTreeView;
   end;
 
   Tfrm_SearchForm = class(TForm)
     lbl_Text: TLabel;
     cb_History: TComboBox;
-    cb_SearchBody: TCheckBox;
-    cb_Regex: TCheckBox;
-    cb_Strict: TCheckBox;
     btn_Ok: TBitBtn;
     btn_Cancel: TBitBtn;
-    cb_FromTop: TCheckBox;
-    procedure cb_SearchBodyClick(Sender: TObject);
+    rb_ClassSearch: TRadioButton;
+    rb_FTS: TRadioButton;
+    cb_SearchFromTheTop: TCheckBox;
+    bvl_Div: TBevel;
+    cb_CompareStrict: TCheckBox;
+    cb_FindFirst: TCheckBox;
+    cb_RegularExpression: TCheckBox;
+    rg_Scope: TRadioGroup;
+    cb_Default: TCheckBox;
     procedure cb_HistoryChange(Sender: TObject);
+    procedure rb_ClassSearchClick(Sender: TObject);
   private
     config: TClassSearch;
+    ahistory: TStrings;
   public
     constructor CreateSearch(AOwner: TComponent; var searchconfig: TClassSearch);
   end;
@@ -69,6 +82,8 @@ var
 
 implementation
 
+uses unit_main;
+
 {$R *.dfm}
 
 function SearchForm(var cs: TClassSearch): boolean;
@@ -78,15 +93,20 @@ begin
     if (result) then begin
       cs.Wrapped := true;
       cs.query := cb_History.Text;
-      cs.isRegex := cb_Regex.Checked;
-      cs.isBodySearch := cb_SearchBody.Checked;
-      cs.isStrict := cb_Strict.Checked;
-      cs.isFromTop := cb_FromTop.Checked;
-      if (config.history.IndexOf(cs.query) = -1) then begin
-        cs.history.Insert(0, cs.query);
-        while (cs.history.Count > 10) do cs.history.Delete(cs.history.Count-1);
+      cs.isRegex := cb_RegularExpression.Checked;
+      cs.isFTS := rb_FTS.Checked;
+      cs.isStrict := cb_CompareStrict.Checked;
+      cs.isFromTop := cb_SearchFromTheTop.Checked;
+      cs.isFindFirst := cb_FindFirst.Checked;
+      cs.Scope := rg_Scope.ItemIndex;
+      if (ahistory.IndexOf(cs.query) = -1) then begin
+        ahistory.Insert(0, cs.query);
+        while (ahistory.Count > 20) do ahistory.Delete(ahistory.Count-1);
       end
-      else cs.history.Move(cs.history.IndexOf(cs.query), 0);
+      else ahistory.Move(ahistory.IndexOf(cs.query), 0);
+      if (cb_Default.Checked) then begin
+				DefaultSC := cs;
+      end;
     end;
     Free;
   end;
@@ -96,28 +116,38 @@ constructor Tfrm_SearchForm.CreateSearch(AOwner: TComponent; var searchconfig: T
 begin
   inherited Create(AOwner);
   config := searchconfig;
-  Caption := config.caption;
-  lbl_Text.Caption := config.text;
   cb_History.Text := searchconfig.query;
   cb_History.SelectAll;
-  cb_History.Items.Assign(config.history);
-  cb_SearchBody.Checked := config.isBodySearch;
-  cb_Regex.Checked := config.isRegex;
-  cb_Strict.Checked := config.isStrict;
-  cb_FromTop.Checked := config.isFromTop;
+  rb_FTS.Checked := config.isFTS;
+  cb_RegularExpression.Checked := config.isRegex;
+  cb_CompareStrict.Checked := config.isStrict;
+  cb_SearchFromTheTop.Checked := config.isFromTop;
+  cb_FindFirst.Checked := config.isFindFirst;
+  rg_Scope.ItemIndex := config.Scope;
+  rb_ClassSearchClick(nil);
+  if (cb_History.Text = '') then begin
+    if (ahistory.Count > 0) then cb_History.Text := ahistory[0];
+  end;
   btn_Ok.Enabled := cb_History.Text <> '';
-  cb_SearchBodyClick(nil); // make sure the init is correct
-end;
-
-procedure Tfrm_SearchForm.cb_SearchBodyClick(Sender: TObject);
-begin
-  cb_Regex.Enabled := cb_SearchBody.Checked;
-  cb_Strict.Enabled := not cb_SearchBody.Checked;
 end;
 
 procedure Tfrm_SearchForm.cb_HistoryChange(Sender: TObject);
 begin
   btn_Ok.Enabled := cb_History.Text <> '';
+end;
+
+procedure Tfrm_SearchForm.rb_ClassSearchClick(Sender: TObject);
+begin
+  cb_SearchFromTheTop.Enabled := rb_ClassSearch.Checked;
+  cb_CompareStrict.Enabled := rb_ClassSearch.Checked;
+
+  cb_FindFirst.Enabled := rb_FTS.Checked;
+  cb_RegularExpression.Enabled := rb_FTS.Checked;
+  rg_Scope.Enabled := rb_FTS.Checked;
+
+  if (rb_ClassSearch.Checked) then ahistory := config.history
+  else ahistory := config.ftshistory;
+  cb_History.Items.Assign(ahistory);
 end;
 
 end.
