6d7b691e86edf7ce24fc5c0a6c0ff9c6110e9474
elmuerte <elmuerte@636d535d-a5df-456e-a4a7-8a432e46c328>
2004-03-20 20:58:27 +0000

    removed obsolete code

--- trunk/UnCodeX/src/unit_definitions.pas
+++ trunk/UnCodeX/src/unit_definitions.pas
@@ -3,7 +3,7 @@
  Author:    elmuerte
  Copyright: 2003, 2004 Michiel 'El Muerte' Hendriks
  Purpose:   General definitions
- $Id: unit_definitions.pas,v 1.87 2004-03-16 08:47:00 elmuerte Exp $
+ $Id: unit_definitions.pas,v 1.88 2004-03-20 20:58:27 elmuerte Exp $
 -----------------------------------------------------------------------------}
 {
     UnCodeX - UnrealScript source browser & documenter
@@ -54,7 +54,7 @@ type
 
 const
   APPTITLE = 'UnCodeX';
-  APPVERSION = '169';
+  APPVERSION = '170';
   {$IFDEF MSWINDOWS}
   APPPLATFORM = 'MS Windows';
   {$ENDIF}
