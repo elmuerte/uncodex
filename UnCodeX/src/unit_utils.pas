da870660fd8c84c124a11876f6b14f0af36c4a0e
Michiel Hendriks <elmuerte@users.noreply.github.com>
2004-03-20 20:58:27 +0000

    removed obsolete code

--- UnCodeX/src/unit_utils.pas
+++ UnCodeX/src/unit_utils.pas
@@ -3,7 +3,7 @@
  Author:    elmuerte
  Copyright: 2003 Michiel 'El Muerte' Hendriks
  Purpose:   General function/utils that require Forms
- $Id: unit_utils.pas,v 1.5 2003-12-15 20:05:54 elmuerte Exp $
+ $Id: unit_utils.pas,v 1.6 2004-03-20 20:58:27 elmuerte Exp $
 -----------------------------------------------------------------------------}
 {
     UnCodeX - UnrealScript source browser & documenter
@@ -47,11 +47,6 @@ type
     property Caption;
   end;
 
-  // Query dialog
-  function SearchQuery(const ACaption, APrompt: string; var value: string; var checkvalue: array of boolean;
-    var history: TStringList; CheckPrompt: array of string): boolean; overload;
-  function SearchQuery(const ACaption, APrompt: string; var value: string; var history: TStringList): boolean; overload;
-
 implementation
 
 { SearchQuery }
@@ -66,111 +61,6 @@ begin
   GetTextExtentPoint(Canvas.Handle, Buffer, 52, TSize(Result));
   Result.X := Result.X div 52;
 end;
-
-function SearchQuery(const ACaption, APrompt: string; var value: string; var checkvalue: array of boolean;
-    var history: TStringList; CheckPrompt: array of string): boolean;
-var
-  Form: TForm;
-  Prompt: TLabel;
-  Edit: TComboBox;
-  CheckBox: array of TCheckBox;
-  DialogUnits: TPoint;
-  ButtonTop, ButtonWidth, ButtonHeight, i, last: Integer;
-begin
-  Result := False;
-  Form := TForm.Create(Application);
-  with Form do
-    try
-      Canvas.Font := Font;
-      DialogUnits := GetAveCharSize(Canvas);
-      BorderStyle := bsDialog;
-      Caption := ACaption;
-      ClientWidth := MulDiv(250, DialogUnits.X, 4);
-      Position := poScreenCenter;
-      Prompt := TLabel.Create(Form);
-      with Prompt do
-      begin
-        Parent := Form;
-        Caption := APrompt;
-        Left := MulDiv(8, DialogUnits.X, 4);
-        Top := MulDiv(8, DialogUnits.Y, 8);
-        Constraints.MaxWidth := MulDiv(234, DialogUnits.X, 4);
-        WordWrap := True;
-      end;
-      Edit := TComboBox.Create(Form);
-      with Edit do
-      begin
-        Parent := Form;
-        Left := Prompt.Left;
-        Top := Prompt.Top + Prompt.Height + 5;
-        Width := MulDiv(234, DialogUnits.X, 4);
-        MaxLength := 255;
-        Text := Value;
-        if (history <> nil) then Items := history;
-        AutoComplete := true;
-        SelectAll;
-      end;
-      last := Edit.Top + Edit.Height;
-      SetLength(CheckBox, High(CheckPrompt)+1);
-      for i := 0 to High(CheckPrompt) do begin
-        CheckBox[i] := TCheckBox.Create(Form);
-        with CheckBox[i] do
-        begin
-          Parent := Form;
-          Left := Prompt.Left;
-          Top := last + 5;
-          Width := MulDiv(234, DialogUnits.X, 4);
-          Caption := CheckPrompt[i];
-          Checked := checkvalue[i];
-          last := Top+Height;
-        end;
-      end;
-      ButtonTop := Last + 10;
-      ButtonWidth := MulDiv(50, DialogUnits.X, 4);
-      ButtonHeight := MulDiv(14, DialogUnits.Y, 8);
-      with TButton.Create(Form) do
-      begin
-        Parent := Form;
-        Caption := SMsgDlgOK;
-        ModalResult := mrOk;
-        Default := True;
-        SetBounds(MulDiv(73, DialogUnits.X, 4), ButtonTop, ButtonWidth,
-          ButtonHeight);
-      end;
-      with TButton.Create(Form) do
-      begin
-        Parent := Form;
-        Caption := SMsgDlgCancel;
-        ModalResult := mrCancel;
-        Cancel := True;
-        SetBounds(MulDiv(127, DialogUnits.X, 4), ButtonTop,
-          ButtonWidth, ButtonHeight);
-        Form.ClientHeight := Top + Height + 13;          
-      end;
-      if ShowModal = mrOk then
-      begin
-        for i := 0 to High(CheckPrompt) do checkvalue[i] := CheckBox[i].Checked;
-        Value := Edit.Text;
-        if (History.IndexOf(Value) = -1) then begin
-          History.Insert(0, Value);
-          while (History.Count > 10) do History.Delete(History.Count-1);
-        end
-        else History.Move(History.IndexOf(Value), 0);
-        Result := True;
-      end;
-    finally
-      Form.Free;
-    end;
-end;
-
-function SearchQuery(const ACaption, APrompt: string; var value: string; var history: TStringList): boolean;
-var
-  tmpb: array[0..0] of boolean;
-  tmps: array[0..0] of string;
-begin
-  result := SearchQuery(ACaption, APrompt, value, tmpb, history, tmps);
-end;
-
 { SearchQuery -- END }
 { TPropertyHintWindow }
 
