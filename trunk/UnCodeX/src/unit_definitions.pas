305963b1011dad4f4f195556aab3e59fb563c5fb
elmuerte <elmuerte@636d535d-a5df-456e-a4a7-8a432e46c328>
2004-04-04 09:04:08 +0000

    fts from selection fix

--- trunk/UnCodeX/src/unit_definitions.pas
+++ trunk/UnCodeX/src/unit_definitions.pas
@@ -3,7 +3,7 @@
  Author:    elmuerte
  Copyright: 2003, 2004 Michiel 'El Muerte' Hendriks
  Purpose:   General definitions
- $Id: unit_definitions.pas,v 1.92 2004-03-30 09:46:17 elmuerte Exp $
+ $Id: unit_definitions.pas,v 1.93 2004-04-04 09:04:08 elmuerte Exp $
 -----------------------------------------------------------------------------}
 {
     UnCodeX - UnrealScript source browser & documenter
@@ -58,7 +58,7 @@ type
 
 const
   APPTITLE = 'UnCodeX';
-  APPVERSION = '172';
+  APPVERSION = '173';
   {$IFDEF MSWINDOWS}
   APPPLATFORM = 'MS Windows';
   {$ENDIF}
