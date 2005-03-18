dd4e6a020296f18a9173574fe62b398c8efa50ea
elmuerte <elmuerte@636d535d-a5df-456e-a4a7-8a432e46c328>
2005-03-18 08:42:20 +0000

    fpc work for ucxcu

--- trunk/UnCodeX/src/unit_fpc_compat.pas
+++ trunk/UnCodeX/src/unit_fpc_compat.pas
@@ -6,7 +6,7 @@
   Purpose:
     FreePascalCompile compatibility unit
 
-  $Id: unit_fpc_compat.pas,v 1.6 2005-03-18 07:43:22 elmuerte Exp $
+  $Id: unit_fpc_compat.pas,v 1.7 2005-03-18 08:42:20 elmuerte Exp $
 *******************************************************************************}
 
 {
@@ -59,10 +59,70 @@ begin
   Result := ForceDirectories(ExtractFilePath(Dir)) and CreateDir(Dir);
 end;
 
+// tries to find a file if the case wasn't correct
+// not 100% the same as Borland's implementation, but it suits out perpose
 function ExpandFileNameCase(const FileName: string; out MatchFound: TFilenameCaseMatch): string;
+var
+  sr: TSearchRec;
+  path, base: string;
+  found: boolean;
+  {$IFDEF LINUX}
+  i: integer;
+  first: string;
+  {$ENDIF}
 begin
-   
-   result := filename;
+  path := ExtractFilePath(filename);
+  base := ExtractFileName(filename);
+  MatchFound := mkNone;
+
+  // first find the path
+  if (not DirectoryExists(path)) then begin
+    path := ExcludeTrailingPathDelimiter(path);
+    path := ExpandFileNameCase(path, MatchFound);
+    if MatchFound = mkNone then exit; // path doesn't exist
+    path := IncludeTrailingPathDelimiter(path);
+  end;
+
+  // try if file exists
+  try
+    if FindFirst(path + base, faAnyFile, sr)= 0 then begin
+      // in case path was corrected
+      if not (MatchFound in [mkSingleMatch, mkAmbiguous]) then MatchFound := mkExactMatch;
+      Result := path + sr.Name;
+      Exit;
+    end;
+  finally
+    FindClose(SR);
+  end;
+
+  found := false;
+
+  {$IFDEF LINUX}
+  first := LowerCase(base[1]);
+  for i := 0 to 1 do begin
+    if (FindFirst(path + first + '*', faAnyFile, SR) = 0) then begin
+      repeat
+        if (CompareText(sr.name, base) = 0) then begin
+          if (found) then begin
+            MatchFound := mkAmbiguous;
+            exit;
+          end
+          else begin
+            result := path+sr.name;
+            found := true;
+          end;
+        end;
+      until (FindNext(sr) <> 0);
+    end;
+    if (UpperCase(base[1]) = first) then break;
+    first := UpperCase(base[1]);
+  end;
+  {$ENDIF}
+
+  if (MatchFound <> mkAmbiguous) then begin
+    if (found) then MatchFound := mkExactMatch
+    else MatchFound := mkNone;
+  end;
 end;
 
 end.
