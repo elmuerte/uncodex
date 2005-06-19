25a976b3e0151cb26dcbc5b0373bbfff376e2b4f
Michiel Hendriks <elmuerte@users.noreply.github.com>
2005-06-19 22:08:29 +0000

    *** empty log message ***

--- UnCodeX/src/unit_sourceparser.pas
+++ UnCodeX/src/unit_sourceparser.pas
@@ -7,7 +7,7 @@
     UnrealScript parser. Used for syntax highlighting, not for analysing.
     Bases on the TParser by Borland.
 
-  $Id: unit_sourceparser.pas,v 1.25 2005-06-16 15:42:14 elmuerte Exp $
+  $Id: unit_sourceparser.pas,v 1.26 2005-06-19 22:08:29 elmuerte Exp $
 *******************************************************************************}
 {
   UnCodeX - UnrealScript source browser & documenter
@@ -41,10 +41,26 @@ type
 
   TProcessMacro = procedure(Sender: TSourceParser);
 
+  TParserState = record
+    StreamPos: integer;
+    Origin: Longint;
+    Buffer: PChar;
+    BufPtr: PChar;
+    BufEnd: PChar;
+    SourcePtr: PChar;
+    SourceEnd: PChar;
+    TokenPtr: PChar;
+    SourceLine: Integer;
+    LinePos: integer;
+    SaveChar: Char;
+    Token: Char;
+  end;
+
   TSourceParser = class(TObject)
   private
     FStream: TStream;
     FOutStream: TStream;
+    FCopyStream: TStringStream;
 
     FOrigin: Longint;
     FBuffer: PChar;
@@ -60,6 +76,8 @@ type
     IsInMComment: boolean;
     commentdepth: integer;
 
+    StateStack: TParserState;
+
     FProcessMacro: TProcessMacro;
 
     procedure ReadBuffer;
@@ -69,6 +87,8 @@ type
   public
     TabIsWS: boolean;
     MacroCallBack: boolean;
+    DoMacro: boolean;
+    FullCopy: boolean;
     constructor Create(Stream, OutStream: TStream; TabIsWhiteSpace: boolean = true);
     destructor Destroy; override;
     procedure CopyTokenToOutput;
@@ -81,6 +101,7 @@ type
     property Token: Char read FToken;
     property OutputStream: TStream read FOutStream write FOutStream;
     property ProcessMacro: TProcessMacro write FProcessMacro;
+    function GetCopyData(flush: boolean = true): string;
 
     procedure PushState;
     procedure PopState;
@@ -115,6 +136,11 @@ begin
   FTokenPtr := FBuffer;
   FSourceLine := 1;
   FToken := ' ';
+  FOrigin := 0;
+  GetMem(StateStack.Buffer, ParseBufSize);
+  DoMacro := true;
+  FullCopy := false;
+  FCopyStream := TStringStream.Create('');
   //SkipToken(True);
 end;
 
@@ -286,18 +312,25 @@ begin
         end;
       '#':
         begin
-          while not (P^ in [#10, toEOF]) do begin
+          if (not DoMacro) then begin
+            Result := P^;
             Inc(P);
             Inc(FLinePos);
-          end;
-          if (P^ = toEOF) then begin
-            Result := toEOF;
           end
           else begin
-            Inc(P);
-            Inc(FSourceLine); // next line
-            FLinePos := 0;
-            Result := toMacro;
+            while not (P^ in [#10, toEOF]) do begin
+              Inc(P);
+              Inc(FLinePos);
+            end;
+            if (P^ = toEOF) then begin
+              Result := toEOF;
+            end
+            else begin
+              Inc(P);
+              Inc(FSourceLine); // next line
+              FLinePos := 0;
+              Result := toMacro;
+            end;
           end;
         end;
       '/':
@@ -348,6 +381,7 @@ begin
   FSourcePtr := P;
   if DoCopy then UpdateOutStream(StartPos);
   FToken := Result;
+  if (FullCopy) then FCopyStream.WriteString(TokenString);
 end;
 
 function TSourceParser.SkipToken(CopyBlanks: Boolean): Char;
@@ -364,10 +398,10 @@ procedure TSourceParser.ReadBuffer;
 var
   Count: Integer;
 begin
-  Inc(FOrigin, FSourcePtr - FBuffer);
-  FSourceEnd[0] := FSaveChar;
-  Count := FBufPtr - FSourcePtr;
-  if Count <> 0 then Move(FSourcePtr[0], FBuffer[0], Count);
+  Inc(FOrigin, FSourcePtr - FBuffer); // increase file origin
+  FSourceEnd[0] := FSaveChar; //
+  Count := FBufPtr - FSourcePtr; // current buff pos - walker pointer = size of processed buffer space
+  if Count <> 0 then Move(FSourcePtr[0], FBuffer[0], Count); // move re
   FBufPtr := FBuffer + Count;
   Inc(FBufPtr, FStream.Read(FBufPtr[0], FBufEnd - FBufPtr));
   FSourcePtr := FBuffer;
@@ -383,6 +417,7 @@ end;
 procedure TSourceParser.SkipBlanks(DoCopy: Boolean);
 var
   Start: PChar;
+  SkipedBlanks: string;
 begin
   Start := FSourcePtr;
   while True do
@@ -391,6 +426,10 @@ begin
       #0:
         begin
           if DoCopy then UpdateOutStream(Start);
+          if (FullCopy) then begin
+            SetString(SkipedBlanks, Start, FSourcePtr - Start);
+            FCopyStream.WriteString(SkipedBlanks);
+          end;
           ReadBuffer;
           if FSourcePtr^ = #0 then Exit;
           Start := FSourcePtr;
@@ -410,6 +449,10 @@ begin
     Inc(FSourcePtr);
   end;
   if DoCopy then UpdateOutStream(Start);
+  if (FullCopy) then begin
+    SetString(SkipedBlanks, Start, FSourcePtr - Start);
+    FCopyStream.WriteString(SkipedBlanks);
+  end;
 end;
 
 function TSourceParser.TokenString: string;
@@ -428,19 +471,53 @@ begin
   OutputStream.WriteBuffer(PChar(data)^, length(data));
 end;
 
+function TSourceParser.GetCopyData(flush: boolean = true): string;
+begin
+  Result := FCopyStream.DataString;
+  if (flush) then begin
+    FCopyStream.Position := 0;
+    FCopyStream.Size := 0;
+  end;
+end;
+
 procedure TSourceParser.PushState;
 begin
-
+  StateStack.StreamPos := FStream.Position;
+  StateStack.Origin := FOrigin;
+  Move(FBuffer[0], StateStack.Buffer[0], ParseBufSize);
+  StateStack.BufPtr := FBufPtr;
+  StateStack.BufEnd := FBufEnd;
+  StateStack.SourcePtr := FSourcePtr;
+  StateStack.SourceEnd := FSourceEnd;
+  StateStack.TokenPtr := FTokenPtr;
+  StateStack.SourceLine := FSourceLine;
+  StateStack.LinePos := FLinePos;
+  StateStack.SaveChar := FSaveChar;
+  StateStack.Token := FToken;
 end;
 
 procedure TSourceParser.PopState;
 begin
-
+  if (FOrigin <> StateStack.Origin) then begin
+    FStream.Seek(StateStack.StreamPos, soFromBeginning);
+    FOrigin := StateStack.Origin;
+    Move(StateStack.Buffer[0], FBuffer[0], ParseBufSize);
+    FBufPtr := StateStack.BufPtr;
+    FBufEnd := StateStack.BufEnd;
+    FSourceEnd := StateStack.SourceEnd;
+    FSaveChar := StateStack.SaveChar;
+  end;
+  FSourcePtr := StateStack.SourcePtr;
+  FTokenPtr := StateStack.TokenPtr;
+  FSourceLine := StateStack.SourceLine;
+  FLinePos := StateStack.LinePos;
+  FToken := StateStack.Token;
+  DiscardState;
 end;
 
 procedure TSourceParser.DiscardState;
 begin
-
+  //
 end;
 
 end.
