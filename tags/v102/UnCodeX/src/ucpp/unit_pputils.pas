c36a92b38eb70f00c6429b3f565970e940d7ae9c
nobody <nobody@636d535d-a5df-456e-a4a7-8a432e46c328>
2005-08-03 07:59:40 +0000

    This commit was manufactured by cvs2svn to create tag 'v102'.

---
+++ tags/v102/UnCodeX/src/ucpp/unit_pputils.pas
@@ -0,0 +1,109 @@
+{*******************************************************************************
+  Name:
+    unit_pputils
+  Author(s):
+    Michiel 'El Muerte' Hendriks
+  Purpose:
+    Various utility functions
+
+  $Id: unit_pputils.pas,v 1.6 2005-06-21 19:55:27 elmuerte Exp $
+*******************************************************************************}
+
+{
+  UCPP - UnrealScript Code PreProcessor
+  Copyright (C) 2005  Michiel Hendriks
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+unit unit_pputils;
+
+{$I ../defines.inc}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+  function GetToken(var input: string; delim: TSysCharSet; nocut: boolean = false): string;
+  procedure ErrorMessage(msg: string);
+  procedure WarningMessage(msg: string);
+  procedure DebugMessage(msg: string);
+  {$IFDEF FPC}
+  function BoolToStr(b : boolean; useString: boolean = false): string;
+  {$ENDIF}
+  function StrRepeat(line: string; count: integer): string;
+
+var
+  ErrorCount: integer = 0;
+  WarnCount: integer = 0;
+  debug_mode: boolean = false;
+
+implementation
+
+function GetToken(var input: string; delim: TSysCharSet; nocut: boolean = false): string;
+var
+  i,j: integer;
+begin
+  i := 1;
+  while ((i <= length(input)) and (input[i] in delim)) do Inc(i);
+  j := i;
+  while ((j <= length(input)) and (not (input[j] in delim))) do Inc(j);
+  result := copy(input, i, j-i);
+  if (not nocut) then begin
+    delete(input, 1, j);
+  end;
+end;
+
+procedure ErrorMessage(msg: string);
+begin
+  Writeln(ErrOutput, 'Error: '+msg);
+  Inc(ErrorCount);
+end;
+
+procedure WarningMessage(msg: string);
+begin
+  Writeln(ErrOutput, 'Warning: '+msg);
+  Inc(WarnCount);
+end;
+
+procedure DebugMessage(msg: string);
+begin
+  if (debug_mode) then Writeln('DEBUG: '+msg);
+end;
+
+{$IFDEF FPC}
+function BoolToStr(b: boolean; useString: boolean = false): string;
+begin
+  if (useString) then begin
+    if (b) then result := 'True'
+    else result := 'False';
+  end
+  else begin
+    if (b) then result := '1'
+    else result := '0';
+  end;
+end;
+{$ENDIF}
+
+function StrRepeat(line: string; count: integer): string;
+begin
+  result := '';
+  while (count > 0) do begin
+    result := result+line;
+    Dec(count);
+  end;
+end;
+
+end.
